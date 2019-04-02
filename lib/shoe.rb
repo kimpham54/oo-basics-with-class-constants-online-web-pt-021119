9d3fc7449af4f75b7f78e8638703cbf536f64694
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-02 05:15:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -22,7 +22,7 @@ BRANDS = []
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    # BRANDS.push(brand)
-end
+#   def brand=(brand)
+#     @brand = brand
+#     # BRANDS.push(brand)
+# end
