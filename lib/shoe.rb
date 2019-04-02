c572ed958b6d157a25b9bf67b2ca290094c72cd1
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-02 05:28:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -25,7 +25,7 @@ BRANDS = []
   def brand=(brand)
     @brand = brand
     # BRANDS.push(brand)
-    ANDS.push(brand)
+    BRANDS.push(brand)
     BRANDS.each do |item|
       if item != brand
         BRANDS.push(brand)
